5ed73a5949c1f81bacda9fbcac42e3e673783764
Promit-revar <promit.revar2211@gmail.com>
2023-03-07 13:02:29 +0530

    Feat: Dockerized project

--- src/utils/insertIntoRedis.js
+++ src/utils/insertIntoRedis.js
@@ -1,10 +1,15 @@
 const redis = require('redis');
 exports.insertIntoRedis = async(token) => {
-  const client = redis.createClient();
+  const config = {
+    socket: {
+      host: "redis",
+      port: 6379,
+    },
+  };
+  const client = redis.createClient(config);
   await client.connect()
   await client.set('token', token);
   const value = await client.get('token');
   await client.disconnect()
-  
   return value;
 }
